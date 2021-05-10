ff3afb222d03ac5b9480d2e475bc38d47f41e56e
asadurzzaman <asadur.dxn2u@gmail.com>
2021-05-11 02:12:44 +0600

    enqueue file created

--- functions.php
+++ functions.php
@@ -136,21 +136,11 @@ function wpxray_widgets_init() {
 }
 add_action( 'widgets_init', 'wpxray_widgets_init' );
 
+
 /**
- * Enqueue scripts and styles.
+ * Implement the Enqueue files.
  */
-function wpxray_scripts() {
-	wp_enqueue_style( 'wpxray-style', get_stylesheet_uri(), array(), _S_VERSION ); 
-
-	wp_style_add_data( 'wpxray-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'wpxray-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'wpxray_scripts' );
+require get_template_directory() . '/inc/enqueue.php';
 
 /**
  * Implement the Custom Header feature.
