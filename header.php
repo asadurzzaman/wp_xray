14e16650c1abd3447fe7141c6c714810b374fa80
asadurzzaman <asadur.dxn2u@gmail.com>
2021-05-11 03:33:00 +0600

    bootstrap col

--- header.php
+++ header.php
@@ -30,37 +30,43 @@
 		<header id="masthead" class="site-header">
 			<div class="container">
 				<div class="row">
-					<div class="site-branding">
-						<?php
-						the_custom_logo();
-						if (is_front_page() && is_home()) :
-						?>
-							<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-						<?php
-						else :
-						?>
-							<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-						<?php
-						endif;
-						$wpxray_description = get_bloginfo('description', 'display');
-						if ($wpxray_description || is_customize_preview()) :
-						?>
-							<p class="site-description"><?php echo $wpxray_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-														?></p>
-						<?php endif; ?>
-					</div><!-- .site-branding -->
+					<div class="col">
+						<div class="site-branding">
+							<?php
+							the_custom_logo();
+							if (is_front_page() && is_home()) :
+							?>
+								<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+							<?php
+							else :
+							?>
+								<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+							<?php
+							endif;
+							$wpxray_description = get_bloginfo('description', 'display');
+							if ($wpxray_description || is_customize_preview()) :
+							?>
+								<p class="site-description"><?php echo $wpxray_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+															?></p>
+							<?php endif; ?>
+						</div><!-- .site-branding -->
+					</div>
+					<div class="col">
+						<nav id="site-navigation" class="main-navigation">
+							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'wpxray'); ?></button>
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'main-menu',
+									'menu_id'        => 'primary-menu',
+								)
+							);
+							?>
+						</nav><!-- #site-navigation -->
+					</div>
 
-					<nav id="site-navigation" class="main-navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'wpxray'); ?></button>
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location' => 'main-menu',
-								'menu_id'        => 'primary-menu',
-							)
-						);
-						?>
-					</nav><!-- #site-navigation -->
+
+
 				</div>
 			</div>
 		</header><!-- #masthead -->
