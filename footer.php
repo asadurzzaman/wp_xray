3347760a9707fcf5e91f83113a12c34d3e406fac
asadurzzaman <asadur.dxn2u@gmail.com>
2021-05-13 00:13:35 +0600

    footer bootstrap add

--- footer.php
+++ footer.php
@@ -13,19 +13,26 @@
 			</div>
 		</div>
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wpxray' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wpxray' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpxray' ), 'wpxray', '<a href="http://asaduzzaman.me">Asad</a>' );
-				?>
-		</div><!-- .site-info -->
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wpxray' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'wpxray' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep"> | </span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpxray' ), 'wpxray', '<a href="http://asaduzzaman.me">Asad</a>' );
+						?>
+				</div><!-- .site-info -->	
+			</div>
+		</div>
+	</div>
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
