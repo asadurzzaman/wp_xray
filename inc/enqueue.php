e43d9051fd2d87ac49ce637a1a2da523e4bb7b31
asadurzzaman <asadur.dxn2u@gmail.com>
2021-05-15 01:04:06 +0600

    15 may change

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -13,16 +13,18 @@
 function wpxray_scripts() {
 	
 	wp_style_add_data( 'wpxray-style', 'rtl', 'replace' );
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.0.0', 'all' );
-    wp_enqueue_style( 'Inter', 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' );
+	wp_enqueue_style('wpxray-google-fonts', wpxray_fonts_url());
+    wp_enqueue_style( 'wpxray-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.0.0', 'all' );
     wp_enqueue_style( 'wpxray-style', get_stylesheet_uri(), array(), _S_VERSION );
 
 
 
 
 	
-    wp_enqueue_script( 'wpxray-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-    wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'wpxray-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'wpxray-custom', get_template_directory_uri() . '/assets/js/custom.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'wpxray-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array(), _S_VERSION, true ); 
+    
     
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '5.0.0', true );
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
